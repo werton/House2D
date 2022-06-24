54dbb904eb595bf90dec603c0fa90b878ff2177b
werton <weerbox@gmail.com>
2022-06-24 18:26:46 +0900

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    private Vector3 _moveDirection;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float shiftX = 0f;
+        float shiftY = 0f;
+        float angle = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            shiftX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            shiftX = +1f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            shiftY = +1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            shiftY = -1f;
+        }
+
+        _moveDirection = new Vector3(shiftX, shiftY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        
+    }
+    void Move()
+    {
+        transform.Rotate(_moveDirection);
+        _rigidbody2D.velocity = _moveDirection * _speed;
+        //_rigidbody2D.MovePosition(transform.position + _moveDirection * _speed * Time.deltaTime);
+
+    }
+}
