ba533878da80693406058a3e80a2d22a706f43e7
werton <weerbox@gmail.com>
2022-06-24 22:52:20 +0900

    Added increase/decrease alarm on enter/exit house

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,50 +8,46 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody2D;
     private Vector3 _moveDirection;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float shiftX = 0f;
-        float shiftY = 0f;
-        float angle = 0f;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            shiftX = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            shiftX = +1f;
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            shiftY = +1f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            shiftY = -1f;
-        }
-
-        _moveDirection = new Vector3(shiftX, shiftY).normalized;
+        Handleinput();
     }
 
     private void FixedUpdate()
     {
         Move();
-        
     }
-    void Move()
+
+    private void Handleinput()
+    {
+        float shiftX = 0.0f;
+        float shiftY = 0.0f;
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            shiftX = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            shiftX = +1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            shiftY = +1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            shiftY = -1.0f;
+        }
+
+        _moveDirection = new Vector3(shiftX, shiftY).normalized;
+    }
+
+    private void Move()
     {
         _rigidbody2D.velocity = _moveDirection * _speed;    
     }
--- Assets/Scripts/Siren.cs
+++ Assets/Scripts/Siren.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,11 +5,75 @@ public class Siren : MonoBehaviour
 {
     [SerializeField] private UnityEvent _catched;
 
+    private AudioSource _audioSource;
+    private bool _isAlarmEnabled = false;
+    private float _minVolume = 0.01f;
+    private float _maxVolume = 1.0f;
+    private float _volumeStep = 0.1f;
+
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = _minVolume;
+        _audioSource.loop = true;
+    }
+
+
+    private void Update()
+    {
+        if (_isAlarmEnabled == true)
+        {
+            if (_audioSource.volume < _maxVolume)
+            {
+                IncreaseVolume();
+            }
+        }
+        else
+        {
+            if (_audioSource.isPlaying == true)
+            {
+
+                if (_audioSource.volume > _minVolume)
+                {
+                    DecreaseVolume();
+                }
+                else
+                {
+                    _audioSource.Stop();
+                }
+            }
+        }
+    }
+
+    private void IncreaseVolume()
+    {
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, _volumeStep * Time.deltaTime);
+    }
+    private void DecreaseVolume()
+    {
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, _volumeStep * Time.deltaTime);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            _catched.Invoke();
+            _isAlarmEnabled = true;
+            _audioSource.Play();
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            if (_isAlarmEnabled == true)
+            {
+                _isAlarmEnabled = false;
+            }
         }
     }
 }
+
